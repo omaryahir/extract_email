8dabd340676b51136a8026a5444cd636a7c7af4c
Omar Mendoza <omaryahir@gmail.com>
2016-01-05 14:03:37 -0600

    20160105--1403

---
+++ extract.py
@@ -0,0 +1,14 @@
+import re
+
+file = open("list_emails.txt")
+for line in file.readlines():
+    file_row=line[0:-1];
+    s = file_row #"John Bolton <john78@mail.com>"
+    s = s.replace(",","")
+    email = re.search('<?([^<>]+)>?$',s).group(0)
+    email = email.replace("<","")
+    email = email.replace(">","")
+
+    #email = ''.join([e for e in s.split() if '@' in e])[1:-1]
+    print email
+file.close()

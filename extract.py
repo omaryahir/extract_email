6ab539796c4b22f95c9d33d4daedbd6b6562a1cb
Omar Mendoza <omaryahir@gmail.com>
2016-01-05 14:05:05 -0600

    ADD: extracting function

--- extract.py
+++ extract.py
@@ -3,7 +3,7 @@ import re
 file = open("list_emails.txt")
 for line in file.readlines():
     file_row=line[0:-1];
-    s = file_row #"John Bolton <john78@mail.com>"
+    s = file_row #"Name Last <namelast@mail.com>"
     s = s.replace(",","")
     email = re.search('<?([^<>]+)>?$',s).group(0)
     email = email.replace("<","")
